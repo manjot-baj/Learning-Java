f93b7ff6f7d540f00afd062eec14eede66a05f9f
manjot-baj <bajwa.mj78@gmail.com>
2024-09-22 18:40:44 +0530

    Variables and datatypes

---
+++ Intro/datatypes.java
@@ -0,0 +1,50 @@
+/*
+
+Datatypes
+
+1. primitve
+------------------------------------------------------------------
+    a.Integer --> Natural Numbers --> byte, short, int, long
+    int ==>> 4bytes
+    long ==>> 8bytes
+    short ==>> 2bytes
+    byte ==>> 1 byte --> 2^7 to 2^7-1 --> -128 to +127
+------------------------------------------------------------------
+    b.Float --> Decimal values --> double, float
+    float ==>> 4 bytes
+    double ==>> 8 bytes --> by default java support this
+
+    double num = 6.5;
+    float num = 6.76f;
+
+-------------------------------------------------------------------
+    c.Character --> Character values --> any 
+    char ==>> 2bytes --> u work with any type of character
+    it supports UNICODE 
+
+    char c = 'k';
+-------------------------------------------------------------------
+    d.Boolean --> it only contains True or False
+
+    boolean b = true;
+
+ */
+
+
+public class datatypes {
+    public static void main(String a[]){
+        int num1 = 2;
+        byte by = 127;
+        short sh = 558;
+        long l = 65542311l;
+
+        float f = 3.6f;
+        double d = 4.7;
+
+        char ch = 'f';
+
+        boolean b = true;
+
+    }
+    
+}
---
+++ Intro/variables.java
@@ -0,0 +1,10 @@
+public class variables {
+    public static void main(String a[]){
+
+        int num1 = 10;
+        int num2 = 20;
+        int result = num1 + num2;
+        System.out.println(result);
+        
+    }
+}
