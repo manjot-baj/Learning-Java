5609135e0b1a461c9cf6ef17f9a143ac095e7988
manjot-baj <bajwa.mj78@gmail.com>
2024-09-24 21:12:06 +0530

    operator

---
+++ Intro/operators.java
@@ -0,0 +1,54 @@
+public class operators {
+    public static void main(String[] args) {
+        
+        // Assignment and Arithmatic operators
+        // int num1 = 9;
+        // int num2 = 3;
+
+        // int result = num1 + num2; // Addition
+        // int result = num1 - num2; // substraction
+        // int result = num1 * num2; // Multiplication
+        // double result = num1 / num2; // division
+        // double result = num1 % num2; // modules
+
+        // num1 = num1 + 3;
+        // num1 = num1 + 1;
+        // num1 +=1;
+        // // increment
+        // num1++; // post increment
+        // ++num1; // pre increment
+        // System.out.println(num1);
+
+        // num1 = num1 - 1;
+        // num1 -=1;
+        // // decrement
+        // num1--; // post decrement
+        // --num1; // pre decrement
+        // System.out.println(num1);
+
+        // System.out.println(result1);
+
+        // Relational operator
+        // <, >, <=, >=, ==, !=
+        // int x = 9;
+        // int y = 9;
+        // boolean r = x == y;
+        // System.out.println(r);
+
+        // logical operator
+        // || , &&, !
+
+        // int x = 7;
+        // int y = 5;
+        // int a = 5;
+        // int b = 9;
+
+        // boolean result = x > y || a > b;
+        // boolean result = x > y && a > b;
+        // System.out.println(result);
+        // System.out.println(!result);
+
+
+    }
+    
+}
