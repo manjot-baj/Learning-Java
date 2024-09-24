5439989a56d89499d0b3fc6205c2ed7c2a7cc83f
manjot-baj <bajwa.mj78@gmail.com>
2024-09-24 22:58:07 +0530

    conditional statement

---
+++ Intro/conditional_statements.java
@@ -0,0 +1,28 @@
+public class conditional_statements {
+    public static void main(String a[]){
+        // if else
+        int x = 12;
+        if(x > 10 && x < 20){
+            System.out.println("Hi");
+        }
+        else{
+            System.out.println("Hello");
+        }
+
+        // if else and else if
+        int y = 5;
+        int z = 12;
+        int w = 4;
+        if(y > z && y > w){
+            System.out.println(y);
+        }
+        else if(z > w){
+            System.out.println(z);
+        }
+        else{
+            System.out.println(w);
+        }
+
+    }
+
+}
