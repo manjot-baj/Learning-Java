bc16b48c8e684185ed74612f0f90add653e8b434
manjot-baj <bajwa.mj78@gmail.com>
2024-09-24 20:30:26 +0530

    type_casting_conversion

---
+++ Intro/type_casting.java
@@ -0,0 +1,16 @@
+/*
+Type conversion and Casting
+Implicit conversion is called Conversion
+Explicit conversion is called Casting
+ */
+
+public class type_casting {
+    public static void main(String[] args) {
+        byte b = 2;
+        int a = 10000;
+        // a = b; /* type conversion */
+        // b = (byte) a; /* type casting */
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
