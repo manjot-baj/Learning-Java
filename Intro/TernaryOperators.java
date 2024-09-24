d636c902ccdbea2f7d200d3b764beceb8cc10a94
manjot-baj <bajwa.mj78@gmail.com>
2024-09-24 23:25:04 +0530

    Ternary operator

--- Intro/TernaryOperators.java
+++ Intro/TernaryOperators.java
@@ -1,6 +1,11 @@
 public class TernaryOperators {
     public static void main(String[] args) {
 
+        // int num = 53;
+        int num = 200;
+        String result = num % 2 == 0 ? "Even" : "Odd";
+        System.out.println(result);
+
     }
 
 }
