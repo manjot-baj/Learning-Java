8e5a4f34dc385f84200654920da82ec9cbb4999e
manjot-baj <bajwa.mj78@gmail.com>
2024-10-12 21:56:04 +0530

    Array

---
+++ OOP/DemoArray.java
@@ -0,0 +1,15 @@
+
+public class DemoArray {
+    public static void main(String a[]) {
+        int nums[] = { 3, 7, 6, 8 };
+        int nums1[] = new int[4];
+        nums1[0] = 3;
+        nums1[1] = 5;
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(nums[i]);
+            System.out.println(nums1[i]);
+        }
+    }
+
+}
